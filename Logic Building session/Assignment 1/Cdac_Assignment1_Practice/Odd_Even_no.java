2c410aa464a506faf06bd26d9a49e73136911172
Shrutika2711 <135197721+Shrutika2711@users.noreply.github.com>
2024-08-23 18:11:05 +0530

    Add files via upload

---
+++ Logic Building session/Assignment 1/Cdac_Assignment1_Practice/Odd_Even_no.java
@@ -0,0 +1,16 @@
+package Cdac_Assignment1_Practice;
+
+public class Odd_Even_no {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 27;
+		if(n % 2 == 0) {
+			System.out.println("Its Even number");
+		}else {
+			System.out.println("Its odd number");
+		}
+		
+	}
+
+}
