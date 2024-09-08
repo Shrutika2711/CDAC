5febed3775c2ecbfab171f38105faab81ddc5370
Shrutika2711 <135197721+Shrutika2711@users.noreply.github.com>
2024-09-09 02:16:11 +0530

    Add files via upload

---
+++ Daily work/JAVA/Assignment2(Sandeep Sir)/Q7.java
@@ -0,0 +1,59 @@
+package Assignment_2;
+
+public class Q7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int bytes = Double.BYTES;
+//		System.out.println("Number of bytes used to represent a double value: " + bytes);//b
+		
+//		double minValue = Double.MIN_VALUE;
+//		double maxValue = Double.MAX_VALUE;
+//		System.out.println("Minimum value of a double: " + minValue);
+//		System.out.println("Maximum value of a double: " + maxValue); //c
+		
+//		double number = 123.456;
+//		String numberAsString = Double.toString(number);
+//		System.out.println("The double value as a String: " + numberAsString);//d
+		
+//		String strNumber = "123.456";
+//		double number = Double.parseDouble(strNumber);
+//		System.out.println("The String value as a double: " + number);//e
+		
+//		String str = "Ab12Cd3";
+//		double b = Double.parseDouble(str);
+//		System.out.println(" string to Double value :" +b);//f
+		
+//		double number = 123.456;
+//		Double numberWrapper = Double.valueOf(number); System.out.println("The double value as a Double object: " + numberWrapper);//g
+		
+//		String strNumber = "123.456";
+//		Double numberWrapper = Double.valueOf(strNumber);
+//		System.out.println("The String value as a Double object: " + numberWrapper);//h
+		
+//		double num1 = 112.3;
+//		double num2 = 984.5;
+//		double sum = Double.sum(num1, num2);
+//		System.out.println("The sum is: " + sum);//i
+		
+//		double num1 = 112.2;
+//		double num2 = 556.6;
+//		double minValue = Double.min(num1, num2);
+//		double maxValue = Double.max(num1, num2);
+//		System.out.println("The minimum value is: " + minValue);
+//		System.out.println("The maximum value is: " + maxValue);//j
+		
+//		double num = -25.0;
+//		double sqrtValue = Math.sqrt(num);
+//		System.out.println("The square root is: " + sqrtValue);//k
+		
+		double num1 = 0.0;
+		double num2 = 0.0;
+		double result = num1 / num2;
+		System.out.println("The result of dividing 0.0 by 0.0 is: " + result);//l
+		
+		
+		
+	}
+
+}
