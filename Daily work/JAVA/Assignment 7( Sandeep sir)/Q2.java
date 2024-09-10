275942e85988d1c23adcf4958cedc007c7f1b6d8
Shrutika2711 <135197721+Shrutika2711@users.noreply.github.com>
2024-09-10 18:05:06 +0530

    Add files via upload

---
+++ Daily work/JAVA/Assignment 7( Sandeep sir)/Q2.java
@@ -0,0 +1,33 @@
+package com.assignment7;
+
+import java.util.Scanner;
+
+public class Q2 {
+	private static void acceptRecord(int[] arr) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 5 integers:");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();   
+        }
+		
+	}
+	private static void printRecord(int[] arr) {
+		// TODO Auto-generated method stub
+		System.out.println("Array elements are:");
+
+        for (int i : arr) {
+            System.out.println(i);  
+        }
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = new int[5];
+        acceptRecord(arr);
+        printRecord(arr);
+
+	}
+
+}
