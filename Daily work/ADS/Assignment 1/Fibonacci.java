067f4758ad43eccf0a2aeff271c9aee51a8d772e
Shrutika2711 <135197721+Shrutika2711@users.noreply.github.com>
2024-09-27 21:47:26 +0530

    Update Fibonacci.java

--- Daily work/ADS/Assignment 1/Fibonacci.java
+++ Daily work/ADS/Assignment 1/Fibonacci.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
 public class Fibonacci {
+    public static int fibo(int n){
+        if(n == 0){
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        return fibo(n - 1) + fibo(n - 2);
+    }
     public static void main(String[] args) {
-    int n1 = 0, n2 = 1;
-    int c;
     Scanner s = new Scanner(System.in);
     System.out.println("Enter number: ");
-    c = s.nextInt();
-    //System.out.println();
-    int nextTerm;
-    for(int i = 2; i < c; i++){
-        nextTerm = n1 + n2;
-        n1 = n2;
-        n2 = nextTerm;
-        System.out.print(nextTerm + "  ");
-
+    int n = s.nextInt();
+    for(int i = 0; i <= n; i++){
+    System.out.print(" " + fibo(i));
     }
     s.close();
     }
-
     
 }
