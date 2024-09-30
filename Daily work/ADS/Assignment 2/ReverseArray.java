206679c5d2b86e86f0f1f3783fb4c2be71b9c6f2
Shrutika2711 <135197721+Shrutika2711@users.noreply.github.com>
2024-09-30 17:50:32 +0530

    Add files via upload

---
+++ Daily work/ADS/Assignment 2/ReverseArray.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter size of list: ");
+        int n = s.nextInt();
+        int[] arr = new int[n];
+
+        System.out.print("Enter numbers: " + " ");
+
+        for(int i = 0; i < n; i++){
+            arr[i] = s.nextInt();
+        }
+        int[] rev = new int[n];
+        for(int i = arr.length - 1; i >= 0 ; i--){
+            System.out.print(arr[i] + " ");
+        }
+        s.close();
+    }
+    
+}
