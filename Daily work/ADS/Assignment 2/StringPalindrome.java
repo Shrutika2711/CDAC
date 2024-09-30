b1833d9cd4eef4c614c638b04cf1ac56cad2eefb
Shrutika2711 <135197721+Shrutika2711@users.noreply.github.com>
2024-09-30 21:44:40 +0530

    Add files via upload

---
+++ Daily work/ADS/Assignment 2/StringPalindrome.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class StringPalindrome {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter String: ");
+        String str = s.nextLine();
+        String rev = "";
+        for(int i = str.length()-1; i >= 0; i--){
+            rev = rev + str.charAt(i);
+        }
+            if(str.equals(rev)){
+                System.out.println("true");
+            }
+            else{
+                System.out.println("false");
+            }
+            s.close();
+    }
+}
